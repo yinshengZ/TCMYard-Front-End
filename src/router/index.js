c4768541c78e3790f4f020e903202db2151e6b35
yinshengZ <zys2701999@gmail.com>
2024-01-04 16:04:29 +0000

    added inventory management page(inventory-manage.vue) and registered it to the route and navigation menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
+import componentsRouter from "./modules/components";
+import chartsRouter from "./modules/charts";
+import tableRouter from "./modules/table";
+import nestedRouter from "./modules/nested";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -40,100 +40,100 @@ import nestedRouter from './modules/nested'
  */
 export const constantRoutes = [
   {
-    path: '/redirect',
+    path: "/redirect",
     component: Layout,
     hidden: true,
     children: [
       {
-        path: '/redirect/:path(.*)',
-        component: () => import('@/views/redirect/index')
-      }
-    ]
+        path: "/redirect/:path(.*)",
+        component: () => import("@/views/redirect/index"),
+      },
+    ],
   },
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
   {
-    path: '/auth-redirect',
-    component: () => import('@/views/login/auth-redirect'),
-    hidden: true
+    path: "/auth-redirect",
+    component: () => import("@/views/login/auth-redirect"),
+    hidden: true,
   },
   {
-    path: '/404',
-    component: () => import('@/views/error-page/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/error-page/404"),
+    hidden: true,
   },
   {
-    path: '/401',
-    component: () => import('@/views/error-page/401'),
-    hidden: true
+    path: "/401",
+    component: () => import("@/views/error-page/401"),
+    hidden: true,
   },
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/dashboard',
+    redirect: "/dashboard",
     children: [
       {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
-        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+        path: "dashboard",
+        component: () => import("@/views/dashboard/index"),
+        name: "Dashboard",
+        meta: { title: "Dashboard", icon: "dashboard", affix: true },
       },
       {
-        path: 'dashboard2',
-        component: () => import('@/views/dashboard/dashboard'),
-        name: 'Dashboard2',
-        meta: { title: 'Dashboard2', icon: 'dashboard', affix: true }
-      }
-    ]
+        path: "dashboard2",
+        component: () => import("@/views/dashboard/dashboard"),
+        name: "Dashboard2",
+        meta: { title: "Dashboard2", icon: "dashboard", affix: true },
+      },
+    ],
   },
 
   {
-    path: '/patient',
+    path: "/patient",
     component: Layout,
-    name: 'Patient',
+    name: "Patient",
     meta: {
-      title: 'Patient',
-      icon: 'el-icon-user'
+      title: "Patient",
+      icon: "el-icon-user",
     },
     children: [
       {
-        path: 'overview',
-        name: 'PatientOverview',
-        component: () => import('@/views/patient/index.vue'),
-        meta: { title: 'Patient Overview', icon: 'overview', affix: true }
+        path: "overview",
+        name: "PatientOverview",
+        component: () => import("@/views/patient/index.vue"),
+        meta: { title: "Patient Overview", icon: "overview", affix: true },
       },
 
       {
-        path: 'search',
-        name: 'SearchPatient',
-        component: () => import('@/views/patient/search.vue'),
-        meta: { title: 'Search Patient', icon: 'search', affix: true }
+        path: "search",
+        name: "SearchPatient",
+        component: () => import("@/views/patient/search.vue"),
+        meta: { title: "Search Patient", icon: "search", affix: true },
       },
       {
-        path: 'add',
-        name: 'AddPatient',
-        component: () => import('@/views/patient/add-patient.vue'),
+        path: "add",
+        name: "AddPatient",
+        component: () => import("@/views/patient/add-patient.vue"),
         meta: {
-          title: 'Add Patient',
-          icon: 'el-icon-document-add',
-          affix: true
-        }
+          title: "Add Patient",
+          icon: "el-icon-document-add",
+          affix: true,
+        },
       },
       {
-        path: 'details/:id',
-        name: 'PatientDetails',
+        path: "details/:id",
+        name: "PatientDetails",
         hidden: true,
-        component: () => import('@/views/patient/PatientDetails.vue'),
+        component: () => import("@/views/patient/PatientDetails.vue"),
         meta: {
-          title: 'Patient Details',
-          icon: 'el-icon-document',
-          affix: true
-        }
-      }
-    ]
+          title: "Patient Details",
+          icon: "el-icon-document",
+          affix: true,
+        },
+      },
+    ],
   },
 
   /*   {
@@ -163,20 +163,20 @@ export const constantRoutes = [
   }, */
 
   {
-    path: '/finance',
+    path: "/finance",
     component: Layout,
-    name: 'Finance',
+    name: "Finance",
     meta: {
-      title: 'Finance',
-      icon: 'finance'
+      title: "Finance",
+      icon: "finance",
     },
     children: [
       {
-        path: 'dashboard',
-        name: 'FinanceDashBoard',
-        component: () => import('@/views/finance/dashboard.vue'),
-        meta: { title: 'Finance', icon: 'dashboard', affix: true }
-      }
+        path: "dashboard",
+        name: "FinanceDashBoard",
+        component: () => import("@/views/finance/dashboard.vue"),
+        meta: { title: "Finance", icon: "dashboard", affix: true },
+      },
 
       /*   {
         path: 'overview',
@@ -185,32 +185,38 @@ export const constantRoutes = [
         meta: { title: 'Finance Overview', icon: 'overview', affix: true }
       },
        */
-    ]
+    ],
   },
 
   {
-    path: '/inventory',
+    path: "/inventory",
     component: Layout,
-    name: 'Inventory',
+    name: "Inventory",
     meta: {
-      title: 'Inventory',
-      icon: 'inventory'
+      title: "Inventory",
+      icon: "inventory",
     },
     children: [
       {
-        path: 'overview',
-        name: 'InventoryOverview',
-        component: () => import('@/views/inventory/index.vue'),
-        meta: { title: 'Inventory Overview', icon: 'inventory', affix: true }
-      }
+        path: "overview",
+        name: "Overview",
+        component: () => import("@/views/inventory/index.vue"),
+        meta: { title: "Inventory Overview", icon: "inventory", affix: true },
+      },
+      {
+        path: "manage",
+        name: "Manage",
+        component: () => import("@/views/inventory/inventory-manage.vue"),
+        meta: { title: "Manage", icon: "el-icon-document", affix: true },
+      },
       /* {
         path:'category',
         name:'Category',
         component:()=>import('@/views/inventory/category.vue'),
         meta:{title:'Category',icon:'category',affix:true}
       } */
-    ]
-  }
+    ],
+  },
 
   /*   {
     path:'/service',
@@ -287,7 +293,7 @@ export const constantRoutes = [
       }
     ]
   } */
-]
+];
 
 /**
  * asyncRoutes
@@ -352,213 +358,213 @@ export const asyncRoutes = [
 
   /** when your routing map is too long, you can split it into small modules **/
 componentsRouter,
-chartsRouter,
-nestedRouter,
-tableRouter,
-{
-  path: '/example',
-  component: Layout,
-  redirect: '/example/list',
-  name: 'Example',
-  meta: {
-    title: 'Example',
-    icon: 'el-icon-s-help'
-  },
-  children: [
-    {
-      path: 'create',
-      component: () => import('@/views/example/create'),
-      name: 'CreateArticle',
-      meta: { title: 'Create Article', icon: 'edit' }
+  chartsRouter,
+  nestedRouter,
+  tableRouter,
+  {
+    path: "/example",
+    component: Layout,
+    redirect: "/example/list",
+    name: "Example",
+    meta: {
+      title: "Example",
+      icon: "el-icon-s-help",
     },
-    {
-      path: 'edit/:id(\\d+)',
-      component: () => import('@/views/example/edit'),
-      name: 'EditArticle',
-      meta: {
-        title: 'Edit Article',
-        noCache: true,
-        activeMenu: '/example/list'
+    children: [
+      {
+        path: "create",
+        component: () => import("@/views/example/create"),
+        name: "CreateArticle",
+        meta: { title: "Create Article", icon: "edit" },
       },
-      hidden: true
-    },
-    {
-      path: 'list',
-      component: () => import('@/views/example/list'),
-      name: 'ArticleList',
-      meta: { title: 'Article List', icon: 'list' }
-    }
-  ]
-},
-{
-  path: '/tab',
-  component: Layout,
-  children: [
-    {
-      path: 'index',
-      component: () => import('@/views/tab/index'),
-      name: 'Tab',
-      meta: { title: 'Tab', icon: 'tab' }
-    }
-  ]
-},
-{
-  path: '/error',
-  component: Layout,
-  redirect: 'noRedirect',
-  name: 'ErrorPages',
-  meta: {
-    title: 'Error Pages',
-    icon: '404'
+      {
+        path: "edit/:id(\\d+)",
+        component: () => import("@/views/example/edit"),
+        name: "EditArticle",
+        meta: {
+          title: "Edit Article",
+          noCache: true,
+          activeMenu: "/example/list",
+        },
+        hidden: true,
+      },
+      {
+        path: "list",
+        component: () => import("@/views/example/list"),
+        name: "ArticleList",
+        meta: { title: "Article List", icon: "list" },
+      },
+    ],
   },
-  children: [
-    {
-      path: '401',
-      component: () => import('@/views/error-page/401'),
-      name: 'Page401',
-      meta: { title: '401', noCache: true }
-    },
-    {
-      path: '404',
-      component: () => import('@/views/error-page/404'),
-      name: 'Page404',
-      meta: { title: '404', noCache: true }
-    }
-  ]
-},
-{
-  path: '/error-log',
-  component: Layout,
-  children: [
-    {
-      path: 'log',
-      component: () => import('@/views/error-log/index'),
-      name: 'ErrorLog',
-      meta: { title: 'Error Log', icon: 'bug' }
-    }
-  ]
-},
-{
-  path: '/excel',
-  component: Layout,
-  redirect: '/excel/export-excel',
-  name: 'Excel',
-  meta: {
-    title: 'Excel',
-    icon: 'excel'
+  {
+    path: "/tab",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/tab/index"),
+        name: "Tab",
+        meta: { title: "Tab", icon: "tab" },
+      },
+    ],
   },
-  children: [
-    {
-      path: 'export-excel',
-      component: () => import('@/views/excel/export-excel'),
-      name: 'ExportExcel',
-      meta: { title: 'Export Excel' }
+  {
+    path: "/error",
+    component: Layout,
+    redirect: "noRedirect",
+    name: "ErrorPages",
+    meta: {
+      title: "Error Pages",
+      icon: "404",
     },
-    {
-      path: 'export-selected-excel',
-      component: () => import('@/views/excel/select-excel'),
-      name: 'SelectExcel',
-      meta: { title: 'Export Selected' }
+    children: [
+      {
+        path: "401",
+        component: () => import("@/views/error-page/401"),
+        name: "Page401",
+        meta: { title: "401", noCache: true },
+      },
+      {
+        path: "404",
+        component: () => import("@/views/error-page/404"),
+        name: "Page404",
+        meta: { title: "404", noCache: true },
+      },
+    ],
+  },
+  {
+    path: "/error-log",
+    component: Layout,
+    children: [
+      {
+        path: "log",
+        component: () => import("@/views/error-log/index"),
+        name: "ErrorLog",
+        meta: { title: "Error Log", icon: "bug" },
+      },
+    ],
+  },
+  {
+    path: "/excel",
+    component: Layout,
+    redirect: "/excel/export-excel",
+    name: "Excel",
+    meta: {
+      title: "Excel",
+      icon: "excel",
     },
-    {
-      path: 'export-merge-header',
-      component: () => import('@/views/excel/merge-header'),
-      name: 'MergeHeader',
-      meta: { title: 'Merge Header' }
-    },
-    {
-      path: 'upload-excel',
-      component: () => import('@/views/excel/upload-excel'),
-      name: 'UploadExcel',
-      meta: { title: 'Upload Excel' }
-    }
-  ]
-},
-{
-  path: '/zip',
-  component: Layout,
-  redirect: '/zip/download',
-  alwaysShow: true,
-  name: 'Zip',
-  meta: { title: 'Zip', icon: 'zip' },
-  children: [
-    {
-      path: 'download',
-      component: () => import('@/views/zip/index'),
-      name: 'ExportZip',
-      meta: { title: 'Export Zip' }
-    }
-  ]
-},
-{
-  path: '/pdf',
-  component: Layout,
-  redirect: '/pdf/index',
-  children: [
-    {
-      path: 'index',
-      component: () => import('@/views/pdf/index'),
-      name: 'PDF',
-      meta: { title: 'PDF', icon: 'pdf' }
-    }
-  ]
-},
-{
-  path: '/pdf/download',
-  component: () => import('@/views/pdf/download'),
-  hidden: true
-},
-{
-  path: '/theme',
-  component: Layout,
-  children: [
-    {
-      path: 'index',
-      component: () => import('@/views/theme/index'),
-      name: 'Theme',
-      meta: { title: 'Theme', icon: 'theme' }
-    }
-  ]
-},
-{
-  path: '/clipboard',
-  component: Layout,
-  children: [
-    {
-      path: 'index',
-      component: () => import('@/views/clipboard/index'),
-      name: 'ClipboardDemo',
-      meta: { title: 'Clipboard', icon: 'clipboard' }
-    }
-  ]
-},
-{
-  path: 'external-link',
-  component: Layout,
-  children: [
-    {
-      path: 'https://github.com/PanJiaChen/vue-element-admin',
-      meta: { title: 'External Link', icon: 'link' }
-    }
-  ]
-},
-// 404 page must be placed at the end !!!
-{ path: '*', redirect: '/404', hidden: true }
+    children: [
+      {
+        path: "export-excel",
+        component: () => import("@/views/excel/export-excel"),
+        name: "ExportExcel",
+        meta: { title: "Export Excel" },
+      },
+      {
+        path: "export-selected-excel",
+        component: () => import("@/views/excel/select-excel"),
+        name: "SelectExcel",
+        meta: { title: "Export Selected" },
+      },
+      {
+        path: "export-merge-header",
+        component: () => import("@/views/excel/merge-header"),
+        name: "MergeHeader",
+        meta: { title: "Merge Header" },
+      },
+      {
+        path: "upload-excel",
+        component: () => import("@/views/excel/upload-excel"),
+        name: "UploadExcel",
+        meta: { title: "Upload Excel" },
+      },
+    ],
+  },
+  {
+    path: "/zip",
+    component: Layout,
+    redirect: "/zip/download",
+    alwaysShow: true,
+    name: "Zip",
+    meta: { title: "Zip", icon: "zip" },
+    children: [
+      {
+        path: "download",
+        component: () => import("@/views/zip/index"),
+        name: "ExportZip",
+        meta: { title: "Export Zip" },
+      },
+    ],
+  },
+  {
+    path: "/pdf",
+    component: Layout,
+    redirect: "/pdf/index",
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/pdf/index"),
+        name: "PDF",
+        meta: { title: "PDF", icon: "pdf" },
+      },
+    ],
+  },
+  {
+    path: "/pdf/download",
+    component: () => import("@/views/pdf/download"),
+    hidden: true,
+  },
+  {
+    path: "/theme",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/theme/index"),
+        name: "Theme",
+        meta: { title: "Theme", icon: "theme" },
+      },
+    ],
+  },
+  {
+    path: "/clipboard",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/clipboard/index"),
+        name: "ClipboardDemo",
+        meta: { title: "Clipboard", icon: "clipboard" },
+      },
+    ],
+  },
+  {
+    path: "external-link",
+    component: Layout,
+    children: [
+      {
+        path: "https://github.com/PanJiaChen/vue-element-admin",
+        meta: { title: "External Link", icon: "link" },
+      },
+    ],
+  },
+  // 404 page must be placed at the end !!!
+  { path: "*", redirect: "/404", hidden: true };
 // ]
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
-  })
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
---
+++ src/views/inventory/inventory-manage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app-container">
+        <inventory-list></inventory-list>
+    </div>
+</template>
+
+
+<script>
+import inventoryList from './components/inventory-list.vue';
+
+export default {
+    components: {
+        inventoryList,
+    },
+    data() {
+        return {
+
+        }
+    },
+
+
+}
+
+
+</script>
+
+<style scoped></style>

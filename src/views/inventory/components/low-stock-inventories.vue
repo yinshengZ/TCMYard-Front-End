7e06f60bcc29f51e7b2b74411f9f7341c44f40bc
yinshengZ <zys2701999@gmail.com>
2024-03-05 17:03:18 +0000

    added update inventory button

--- src/views/inventory/components/low-stock-inventories.vue
+++ src/views/inventory/components/low-stock-inventories.vue
@@ -13,15 +13,31 @@
                 </el-table-column>
 
                 <el-table-column label="Stocks">
+
                     <template slot-scope="{row}">
                         <div>
                             <span>{{ row.stock }}</span>
                         </div>
                     </template>
                 </el-table-column>
+
+                <el-table-column>
+
+
+                    <template slot-scope="{row}">
+                        <div>
+                            <el-button type="primary" @click="load_update_form(row.id)"> <svg-icon
+                                    icon-class="edit"></svg-icon> Update</el-button>
+                        </div>
+                    </template>
+                </el-table-column>
             </el-table>
 
         </el-card>
+
+        <el-dialog :visible.sync="update_form_loaded" :before-close="get_data">
+            <update-inventory-form :key="key" :inventory_id="inventory_id"></update-inventory-form>
+        </el-dialog>
     </div>
 </template>
 
@@ -29,11 +45,18 @@
 <script>
 
 import { get_lowest_stocks } from "@/api/inventory"
+import updateInventoryForm from "./update-inventory-form.vue"
 
 export default {
+    components: {
+        updateInventoryForm
+    },
     data() {
         return {
             inventory: [],
+            key: 0,
+            inventory_id: '',
+            update_form_loaded: false,
         }
     },
 
@@ -42,10 +65,18 @@ export default {
     },
 
     methods: {
+        load_update_form(id) {
+
+            this.inventory_id = id
+            this.update_form_loaded = true
+            this.key++
+        },
+
         get_data() {
             get_lowest_stocks().then((response) => {
                 this.inventory = response.data
             })
+            this.update_form_loaded = false
         }
     }
 }

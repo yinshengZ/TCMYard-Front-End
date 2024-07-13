0aa52dfb11adabb0a48c2d5aa1336dcf8844b69b
yinshengZ <zys2701999@gmail.com>
2024-07-13 15:46:13 +0100

    added patient-gender-trends.vue component to display trends patients' gender that joined either by year or months intervals

---
+++ src/views/patient/components/stat-components/patients-stat/components/patient-gender-trends.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <el-tabs>
+            <el-tab-pane label="By Year">
+                <patients-gender-by-year></patients-gender-by-year>
+            </el-tab-pane>
+            <el-tab-pane label="By Month">
+                <patients-gender-by-month></patients-gender-by-month>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+
+<script>
+import PatientsGenderByMonth from './patients-gender-by-month.vue'
+import PatientsGenderByYear from './patients-gender-by-year.vue'
+
+export default {
+    components: {
+        PatientsGenderByMonth,
+        PatientsGenderByYear
+    }
+}
+
+</script>
+
+
+<style lang="scss" scoped></style>
